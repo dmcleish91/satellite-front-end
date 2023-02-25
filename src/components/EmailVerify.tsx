c6b19b87ac1ec9b810be8673af53f2cb8ba04686
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-02-25 15:00:56 -0500

    chore: styling cleanup

--- src/components/EmailVerify.tsx
+++ src/components/EmailVerify.tsx
@@ -1,15 +1,18 @@
-import { Button, Input } from '@geist-ui/core';
+import { Button, Input, Link } from '@geist-ui/core';
 import { Mail } from 'react-feather';
 
 export const EmailVerify = () => {
   return (
     <div className='flex flex-col items-center justify-center h-[1110px] space-y-2'>
-      <p className='text-4xl font-bold tracking-normal mb-6'>Log in to Continue</p>
+      <p className='text-3xl font-bold tracking-normal mb-7'>Log in to Continue</p>
       <Input placeholder='Email Address' scale={1.5} width='300px' />
       {/* <Input.Password placeholder='Password' scale={1.5} width='300px' /> */}
-      <Button icon={<Mail />} type='success' scale={1.5} width='300px'>
+      <Button icon={<Mail />} type='success' scale={1.5} width='300px' style={{ marginBottom: '24px' }}>
         <p className='font-medium'>Continue with Email</p>
       </Button>
+      <Link href='#' color>
+        ← Other Login Options
+      </Link>
     </div>
   );
 };
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,15 +3,15 @@ import { Gitlab } from 'react-feather';
 
 export const Header = () => {
   return (
-    <div className='flex flex-row justify-between items-center border-b-2 border-gray-200 h-[64px] p-4'>
+    <div className='flex flex-row justify-between items-center border-b-2 border-gray-200 h-[64px] px-[27%]'>
       <div className='flex flex-row items-center space-x-2'>
         <Gitlab size={24} /> <p className='text-xl font-bold'>Gitlabs Mockup</p>
       </div>
-      <div className='flex items-center space-x-2'>
-        <Link href='#' scale={2 / 3} style={{ fontSize: '12px' }}>
+      <div className='flex items-center space-x-4'>
+        <Link href='#' style={{ fontSize: '14px', fontWeight: '500' }}>
           Contact
         </Link>
-        <Button auto scale={2 / 3}>
+        <Button auto scale={2 / 3} style={{ fontSize: '14px', fontWeight: '500' }}>
           Sign in
         </Button>
       </div>
